c4c57363259cfa199e5c0e39ec6c00e3b75ddfde
Hagen Glötter <github@dg7shg.de>
2023-01-01 23:41:28 +0100

    Create base-setup.sh

---
+++ base-setup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "pi  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/casper
+echo "hagen  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/casper
+
+sudo apt update
+sudo apt upgrade
+sudo apt install net-tools colordiff figlet toilet git 
+git clone https://github.com/hagen-gloetter/Linux-Console-Startup.git
+cat Linux-Console-Startup/bash.bashrc >> /etc/bash.bashrc
+
+adduser pi
